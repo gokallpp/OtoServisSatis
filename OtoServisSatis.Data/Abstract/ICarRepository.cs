a77be88ae72650aa44db2e435482d99f606dfa6c
Gokalp <gokalppabdullah@gmail.com>
2026-07-17 23:03:11 +0300

    Car Repository

---
+++ OtoServisSatis.Data/Abstract/ICarRepository.cs
@@ -0,0 +1,11 @@
+﻿using OtoServisSatis.Entities;
+
+namespace OtoServisSatis.Data.Abstract
+{
+    public interface ICarRepository : IRepository<Arac>
+    {
+        Task<IEnumerable<Arac>> GetCustomCarsList();
+        Task<Arac> GetCustomCar(int id);
+    }
+}
+
---
+++ OtoServisSatis.Data/Concrete/CarRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OtoServisSatis.Data.Abstract;
+using OtoServisSatis.Entities;
+
+namespace OtoServisSatis.Data.Concrete
+{
+    internal class CarRepository : Repository<Arac>, ICarRepository
+    {
+        public CarRepository(DatabaseContext context) : base(context)
+        {
+        }
+
+        public async Task<Arac> GetCustomCar(int id)
+        {
+            // Arac nesnesini alırken Marka gezinme özelliğini dahil edin
+            return await _dbSet.AsNoTracking().Include(x => x.Marka).FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<IEnumerable<Arac>> GetCustomCarsList()
+        {
+            // Tüm araçları alırken Marka gezinme özelliğini dahil edin
+            return await _dbSet.AsNoTracking().Include(x => x.Marka).ToListAsync();
+        }
+
+        
+    }
+}
